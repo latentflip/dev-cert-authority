6f4bd3b923027401c110654a289dab160a945889
Philip Roberts <phil@latentflip.com>
2016-07-11 11:56:58 +0100

    Return key contents so can be passed straight to http

--- sign-cert.js
+++ sign-cert.js
@@ -29,7 +29,7 @@ module.exports = function (host) {
   }
 
   return {
-    key: certKeyPath,
-    cert: certCrtPath
+    key: Fs.readFileSync(certKeyPath),
+    cert: Fs.readFileSync(certCrtPath)
   };
 };
